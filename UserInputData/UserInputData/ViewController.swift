4898ea23c5f33002d6bbfc302f90d36d1c6b59f4
rbruner <rodrigo@bruner.net.br>
2024-05-24 22:35:42 -0700

    chore: create the project

---
+++ UserInputData/UserInputData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserInputData
+//
+//  Created by user249117 on 5/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
